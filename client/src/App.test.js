6328d4d359895b7e0fdac589272a794664cef4f7
Amit Singh <kunwarofficial012023@gmail.com>
2024-06-18 21:25:37 +0530

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+import {act} from 'react';
+
+it('render without crashing...', () => {});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+

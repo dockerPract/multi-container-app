64eea894f76ee5c4c57fdf9c8e5f7e8165047b84
Amit Singh <kunwarofficial012023@gmail.com>
2024-06-24 19:46:34 +0530

    last commit

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            FIBONACCI CALCULATOR
+            FIBONACCI CALCULATOR VERSION 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
